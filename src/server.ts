e1e490ea6fd739945a3ec9d42177a39a615a5547
Ahnaf Abid <ahnafabid600@gmail.com>
2026-08-10 23:31:47 +0600

    feat: integrate Prisma client with PostgreSQL setup and update server connection logic

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import app from "./app"
+import { prisma } from "./lib/prisma"
 
 const PORT = process.env.PORT || 5000
 
 async function main(){
     try {
+        // await prisma.$connect()
+        console.log("Database connected successfully")
         app.listen(PORT, ()=>{
             console.log(`server is running on port ${PORT}` )
         })
     } catch (error) {
         console.error("Error starting the server:", error)
+        // await prisma.$disconnect()
         process.exit(1)
     }
 }
