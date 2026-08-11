f68cf471f0bb7533490473a8c1b6537cb8dfef0a
Ahnaf Abid <ahnafabid600@gmail.com>
2026-08-12 00:06:27 +0600

    feat: initialize Express application with middleware and health check route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import express,{ Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 
 
 const app: Application = express()
@@ -15,7 +16,7 @@ app.use(cors({
 }))
 
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
     res.send("Hello World!")
 })
 
