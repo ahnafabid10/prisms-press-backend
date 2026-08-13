dd5d9b0588cb08512e89e8a2b1edb6cbe225a009
Ahnaf Abid <ahnafabid600@gmail.com>
2026-08-13 17:51:55 +0600

    feat: implement standardized response utility and user registration controller

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -2,21 +2,21 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
+const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body
 
-const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction)=>{
-     const payload = req.body
+    const user = await userService.registerUserIntoDB(payload)
 
-        const user = await userService.registerUserIntoDB(payload)
-
-        res.status(httpStatus.CREATED).json({
-            success: true,
-            status: httpStatus.CREATED,
-            message: "User registered successfully",
-            data: {
-                user
-            }
-        })
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "User registered successfully",
+        data: {
+            user
+        }
+    })
 })
 
 export const userController = {
