e3e437ed0669e0a31721ea15adb167e917ba6d7e
Ahnaf Abid <ahnafabid600@gmail.com>
2026-08-13 12:02:29 +0600

    feat: implement error handling middleware for user registration and refactor controller

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,33 +1,23 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
 
-const registerUser = async (req: Request, res: Response) => {
-try {
-        const payload =req.body
+const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction)=>{
+     const payload = req.body
 
-   const user = await userService.registerUserIntoDB(payload)
+        const user = await userService.registerUserIntoDB(payload)
 
-    res.status(httpStatus.CREATED).json({ 
-        success: true,
-        status: httpStatus.CREATED,
-        message: "User registered successfully" ,
-        data: {
-            user
-        }
-    })
-} catch (error) {
-    console.error("Error registering user:", error)
-
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        status: httpStatus.INTERNAL_SERVER_ERROR,
-        message: "Error registering user",
-        error: error instanceof Error ? error.message : "Unknown error"
-    })
-}
-}
+        res.status(httpStatus.CREATED).json({
+            success: true,
+            status: httpStatus.CREATED,
+            message: "User registered successfully",
+            data: {
+                user
+            }
+        })
+})
 
 export const userController = {
     registerUser
---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, RequestHandler, Response } from "express"
+import httpStatus from "http-status";
+
+export const catchAsync = (Fn: RequestHandler) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await Fn(req, res, next)
+        } catch (error) {
+            console.error("Error registering user:", error)
+
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                success: false,
+                status: httpStatus.INTERNAL_SERVER_ERROR,
+                message: "Error registering user",
+                error: error instanceof Error ? error.message : "Unknown error"
+            })
+        }
+    }
+}
