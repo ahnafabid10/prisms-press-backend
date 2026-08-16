30b6ec0bac23f83a6e9d3ebded8228cfbb108423
Ahnaf Abid <ahnafabid600@gmail.com>
2026-08-16 23:53:01 +0600

    feat: integrate JWT utility for token creation in user authentication

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,6 +1,10 @@
+
+import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { jwtUtils } from "../../utils/jwt";
 import { ILoginUser } from "./auth.interface"
 import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 const loginUser = async (payload: ILoginUser) => {
     const { email, password } = payload;
@@ -13,16 +17,42 @@ const loginUser = async (payload: ILoginUser) => {
     // }
 
     const user = await prisma.user.findUniqueOrThrow({
-        where: {email}
+        where: { email }
     })
 
     const isPasswordMatched = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordMatched) {
+    if (!isPasswordMatched) {
         throw new Error("Password is incorrect");
     }
 
-    return user
+    const jwtPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+    }
+
+
+
+//     const accessToken = jwt.sign(jwtPayload, config.jwt_access_token, {
+//         expiresIn: config.jwt_access_expires_in
+//     } as SignOptions
+// ) 
+
+const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_token,
+    config.jwt_access_expires_in as SignOptions
+)
+
+    const refreshToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_refresh_token,
+        config.jwt_refresh_expires_in as SignOptions
+    )
+
+    return { accessToken, refreshToken }
 }
 
 export const userService = {
